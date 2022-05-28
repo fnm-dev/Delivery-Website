d06f8a0198b98605dcb3b93b792b055b90a34d3e
Felipe <f_nmachado@hotmail.com>
2022-05-28 15:52:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php
-  session_start();
-  //Linha 135
-  if(!isset($_SESSION["logado"])){
-      $_SESSION["logado"]=NULL;
-  }
+    session_start();
   
-  if(isset($_SESSION["logado"]) || $_SESSION["logado"] == TRUE){
-    $path="./dashboard/index.php";
-    $status="Painel";
-    $logout=TRUE;
-}else{
-    $path="./dashboard/login.php";
-    $status="Login";
-    $logout=FALSE;
-}
+     //Linha 135
+    if(!isset($_SESSION["logado"])){
+         $_SESSION["logado"]=NULL;
+    }
+  
+    if(isset($_SESSION["logado"]) || $_SESSION["logado"] == TRUE){
+        $path="./dashboard/index.php";
+        $status="Painel";
+        $logout=TRUE;
+    }else{
+        $path="./dashboard/login.php";
+        $status="Login";
+        $logout=FALSE;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
+<?php
+    //include 'enviaWhatsapp.php';
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Loanday Template">
@@ -46,7 +49,6 @@
     <div id="preloder">
         <div class="loader"></div>
     </div>
-
     <!-- Offcanvas Menu Begin -->
     <div class="offcanvas-menu-overlay"></div>
     <div class="offcanvas-menu-wrapper">
@@ -60,6 +62,9 @@
             <ul>
                 <li class="active"><a href="./index.php">Home</a></li>
                 <li><a href="#colab">Colaboradores</a></li>
+                <li><a href="./cardapio.php">Cardápio</a></li>
+                <li><a href="fornecedores.php">Fornecedores</a></li>
+                <li><?php echo "<a href=".$path.">".$status."</a>" ?></li>
             </ul>
         </nav>
         <div id="mobile-menu-wrap"></div>
@@ -72,13 +77,13 @@
                     $ano = date('Y');
                     $dia = array('Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado');
                     $mes = array('', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
-                    echo "<br>Jundiaí, ".$dia_mes . " de " . $mes[$numero_mes] . " de " . $ano . " Horário ".date('H:i', $_SESSION['hora']);
+                    echo "<br>Jundiaí, ".$dia_mes . " de " . $mes[$numero_mes] . " de " . $ano . " Horário ".date('H:i');
                 ?></li>
             <li><i class="fa fa-phone"></i> (11) 4567-8910</li>
             <li><i class="fa fa-envelope"></i> brothersburgue@gmail.com</li>
         </ul>
         <div class="offcanvas__language">
-            <img src="img/flag.png" alt="">
+            <img src="img/bra.png" alt="">
             <span>Portugues/BR</span>
             <i class="fa fa-angle-down"></i>
             <ul>
@@ -99,9 +104,9 @@
                     <?php date_default_timezone_set('America/Sao_Paulo');echo nl2br("Av. Lanches, Nº1029 - Jundiaí/SP <br>");
                     ?>
                     </li>
-                            <li><i class="fa fa-phone"></i> (11) 4567-8910</li>
-                            <li><i class="fa fa-envelope"></i> brothersburgue@gmail.com</li>
-                            <li><i class="fa fa-calendar"></i>
+                        <li><i class="fa fa-phone"></i> (11) 4567-8910</li>
+                        <li><i class="fa fa-envelope"></i> brothersburgue@gmail.com</li>
+                        <li><i class="fa fa-calendar"></i>
                     <?php
                     $numero_dia = date('w')*1;
                     $dia_mes = date('d');
@@ -143,7 +148,6 @@
                                 <li><a href="fornecedores.php">Fornecedores</a></li>
                                 <li><?php echo "<a href=".$path.">".$status."</a>" ?></li>
                             </ul>
-                            
                         </nav>
                         <?php if($logout==TRUE) echo '<a href="./dashboard/logout.php"><div class="header__search"><i class="fa-solid fa-arrow-right-from-bracket"></i></div></a>' ?>
                         &nbsp;&nbsp;&nbsp;&nbsp;
@@ -172,7 +176,7 @@
                 <div class="col-lg-5 offset-lg-2">
                     <div class="hero__form">
                         <h3></h3>
-                        <form action="#">
+                        <form>
                             <div class="input-list">
                                 <div class="input-list-item">
                                     <p>Nome:</p>
@@ -206,6 +210,15 @@
                                 </select>
                             </div>
                             <button type="submit" class="site-btn">Realizar Pedido!</button>
+                            <div class="form-group mb-2">
+                            <label>Enviar</label>
+                            <div class="col px-0">
+                                <button id="by-link" type="button" class="btn btn-info">Link</button>
+                                <button id="by-popup" type="button" class="btn btn-info">PopUp</button>
+                                <hidden id="phone" value="5511942036449"></hidden>
+                                <hidden id="message" value="abc"></hidden>
+                            </div>
+                        </div>
                         </form>
                     </div>
                 </div>
@@ -290,7 +303,7 @@
                         <h5></h5>
                         <div class="footer__widget">
                             <ul>
-                                <li><a href="#"></a></li>
+                                <li><a href="whatsapp-send.html">aaaaa</a></li>
                                 <li><a href="#"></a></li>
                                 <li><a href="#"></a></li>
                                 <li><a href="#"></a></li>
@@ -351,6 +364,91 @@
     <script src="js/jquery.slicknav.js"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
+    <script>
+        let phone = document.getElementById('phone')
+        let message = document.getElementById('message')        
+
+        // buttons
+        let linkHandler = document.getElementById('by-link')
+        let popUpHandler = document.getElementById('by-popup')        
+
+        // font: https://detectmobilebrowsers.com
+        let isMobile = (function(a) {
+            if ( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)) ) {
+                return true
+            } else {
+                return false
+            }
+        })(navigator.userAgent || navigator.vendor || window.opera)
+
+
+        const makeLink = function(mode) {        
+
+            let mount = function() {
+                if ( isMobile ) {
+                    let target = `whatsapp://send?`
+                    if ( !!phone && phone.value !== '' ) {
+                        target += `phone=${encodeURIComponent(phone.value)}&`
+                    }
+                    if ( !!message && message.value !== '' ) {
+                        target += `text=${encodeURIComponent(message.value)}`
+                    }
+                    return target
+                } else {
+                    let target = `https://api.whatsapp.com/send?`
+                    if ( !!phone && phone.value !== '' ) {
+                        target += `phone=${encodeURIComponent(phone.value)}&`
+                    }
+                    if ( !!message && message.value !== '' ) {
+                        target += `text=${encodeURIComponent(message.value)}`
+                    }
+                    return target
+                }
+            
+            }        
+
+            let openLink = function() {
+                let a = document.createElement('a')
+                a.target = '_blank'
+                a.href = mount()
+                a.click()
+                console.log('By Link')
+            }        
+
+            let openPopUp = function() {
+                let h = 650,
+                    w = 550,
+                    l = Math.floor(((screen.availWidth || 1024) - w) / 2),
+                    t = Math.floor(((screen.availHeight || 700) - h) / 2)
+                // open popup
+                let options = `height=600,width=550,top=${t},left=${l},location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=0`
+                let popup = window.open(mount(), 'self', options)
+                if ( popup ) {
+                    popup.focus()
+                }
+                console.log('By PopUp')
+            }
+            
+            switch (mode) {
+                case 'link':
+                    openLink()
+                break
+                case 'popup':
+                    openPopUp()
+                break
+            }
+        } 
+
+        // events handler(s)
+        linkHandler.addEventListener('click', function(e) {
+            makeLink('link')
+        }, false)
+        popUpHandler.addEventListener('click', function(e) {
+            makeLink('popup')
+        }, false)
+    </script>
 </body>
 
 </html>
