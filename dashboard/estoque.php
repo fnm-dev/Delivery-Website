1dd0a9ed1a5f638e1a7680075385b75f440ac45a
Felipe <f_nmachado@hotmail.com>
2022-05-28 15:54:17 -0300

    Update estoque.php

--- dashboard/estoque.php
+++ dashboard/estoque.php
@@ -1,3 +1,16 @@
+<?php
+//INICIO A SESSÃO
+session_start();
+ 
+//Verifico se o usuário está logado no sistema
+if (!isset($_SESSION["logado"]) || $_SESSION["logado"] != TRUE) {
+    header("Location: login.php?erro=2");
+}
+else {
+    echo "<h1>Seja bem-vindo, ".$_SESSION["user"]."</h1>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <?php
