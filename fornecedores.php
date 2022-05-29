a4a804ee2debd1620b1983090ec1382dbf02ef05
Felipe <f_nmachado@hotmail.com>
2022-05-29 14:37:02 -0300

    Update fornecedores.php

--- fornecedores.php
+++ fornecedores.php
@@ -7,7 +7,7 @@
         <meta name="keywords" content="Loanday, unica, creative, html">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Brothers | Peça já!</title>
+        <title>Brothers | Fornecedores</title>
         <?php include_once("./partials/styles.php") ?>
     </head>
     <body>
