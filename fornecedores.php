5c58fd45b53c0d15bc2c2f7ac050c99766e94efa
Felipe <f_nmachado@hotmail.com>
2022-05-30 20:45:44 -0300

    Update fornecedores.php

--- fornecedores.php
+++ fornecedores.php
@@ -13,29 +13,38 @@
     <body>
         <?php include_once("./partials/header_mobile.php") ?>
         <?php include_once("./partials/header.php") ?>
-        <div style="text-align: center; font-size: 20px;">
-        <?php 
-            $link = "./xml/fornecedores.xml"; 
-            $xml = simplexml_load_file($link) -> fornecedores;
+        <div class="container">
+			<h2>Fornecedores</h2>
+			<div class="row">
+				<div class="col-md-12">
+					 <div class="table-responsive">
+                     <div style="text-align: center; font-size: 20px;">
+                        <?php 
+                            $link = "./xml/fornecedores.xml"; 
+                            $xml = simplexml_load_file($link) -> fornecedores;
 
-            foreach($xml -> fornecedor1 as $fornecedor1){ 
-                echo "<strong>Nome:</strong> ".utf8_decode($fornecedor1 -> nome)."<br />"; 
-                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor1 -> telefone1)."<br />";
-                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor1 -> telefone2)."<br />";
-                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor1 -> email1)."<br />";
-                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor1 -> email2)."<br />";
-                echo "<br />"; 
-            } 
-                foreach($xml -> fornecedor2 as $fornecedor2){ 
-                echo "<strong>Nome:</strong> ".utf8_decode($fornecedor2 -> nome)."<br />"; 
-                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor2 -> telefone1)."<br />";
-                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor2 -> telefone2)."<br />";
-                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor2 -> email1)."<br />";
-                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor2 -> email2)."<br />";
-                echo "<br />"; 
-            } 
-        ?>
-        </div>
+                            foreach($xml -> fornecedor1 as $fornecedor1){ 
+                                echo "<strong>Nome:</strong> ".utf8_decode($fornecedor1 -> nome)."<br />"; 
+                                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor1 -> telefone1)."<br />";
+                                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor1 -> telefone2)."<br />";
+                                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor1 -> email1)."<br />";
+                                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor1 -> email2)."<br />";
+                                echo "<br />"; 
+                            } 
+                                foreach($xml -> fornecedor2 as $fornecedor2){ 
+                                echo "<strong>Nome:</strong> ".utf8_decode($fornecedor2 -> nome)."<br />"; 
+                                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor2 -> telefone1)."<br />";
+                                echo "<strong>Telefone:</strong> ".utf8_decode($fornecedor2 -> telefone2)."<br />";
+                                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor2 -> email1)."<br />";
+                                echo "<strong>E-mail:</strong> ".utf8_decode($fornecedor2 -> email2)."<br />";
+                                echo "<br />"; 
+                            } 
+                        ?>
+                        </div>
+					 </div>
+				</div>
+			</div>
+		</div>
         <?php include_once("./partials/footer.php") ?>
         <?php include_once("./partials/search.php") ?>
         <?php include_once("./partials/scripts.php") ?>
