f5ec60de4bf2233a0d3beb89800c0e5f6fd25ad1
Felipe <f_nmachado@hotmail.com>
2022-06-04 14:09:36 -0300

    Update conexao.php

--- dashboard/conexao.php
+++ dashboard/conexao.php
@@ -1,3 +1,3 @@
 ﻿<?php
-    $conexao = mysqli_connect("localhost","root","","delivery") or die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
+    $conexao = mysqli_connect("us-cdbr-east-05.cleardb.net","b1a9352596012f","a9a8d9a2","heroku_9f6606b11473bf0") or die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
 ?>
